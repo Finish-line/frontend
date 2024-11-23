4da4a13308ebaa96e114a075f77a200c84c0e45c
Niklas Burghardt <niklas.burghardt@web.de>
2024-11-23 16:14:07 +0100

    Login working properly

--- app/(main)/(tabs)/settings.tsx
+++ app/(main)/(tabs)/settings.tsx
@@ -1,14 +1,25 @@
+import LoginScreen from "@/app/(auth)/login";
+import { magicAuth } from "@/auth/auth";
+import Button from "@/components/button";
 import SinglePersonMarker from "@/components/marker/single-person";
+import { Redirect, router } from "expo-router";
 import React, { useRef } from "react";
 import { View } from "react-native";
 import MapView from "react-native-maps";
 
 export default function SettingsScreen() {
+  const logout = async () => {
+    try {
+      await magicAuth.user.logout();
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <View style={{ height: "100%", width: "100%" }}>
-      <MapView rotateEnabled={false} style={{ flex: 1 }}>
-        <SinglePersonMarker />
-      </MapView>
+      <Button text="Show Info" onPress={async () => magicAuth.wallet.showBalances()}/>
+        <Button text={"Logout"} onPress={async() => await logout()} />
     </View>
   );
 }
